c80cff219caef14c5ef89c4fe04ecbdda36a6740
Ian Jackson <iansjackson8@gmail.com>
2023-08-23 19:51:04 -0400

    newline after enter

--- kernel/comhand.c
+++ kernel/comhand.c
@@ -7,4 +7,6 @@ void comhand(void)
     {
     	char buf[100] = { 0 };
         int nread = sys_req(READ, COM1, buf, sizeof(buf));
-        sys_req(WRITE, COM1, buf, nread)
+        sys_req(WRITE, COM1, buf, nread);
+    }
+}
