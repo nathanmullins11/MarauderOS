f923f4ff4656700e081e724bd63bf96999a3da6b
Ian Jackson <iansjackson8@gmail.com>
2023-08-23 10:46:24 -0400

    a lil work on comhand

--- kernel/comhand.c
+++ kernel/comhand.c
@@ -1,9 +1,14 @@
 #include <comhand.h>
+#include <sys_req.h>
 
 void comhand(void)
 {
     for ( ;; ) 
     {
-        
+        char buf[100] = { 0 };
+        int nread = sys_req(READ, COM1, buf, sizeof(buf));
+        sys_req(WRITE, COM1, buf, nread);
+
+        if ()
     }
 }
