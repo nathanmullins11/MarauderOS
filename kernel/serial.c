a8d3caf6a1c506cc4806aa16a46c3fb87f1e6784
nathanmullins11 <ndm00002@mix.wvu.edu>
2023-08-25 13:29:02 -0400

    made changes in main oops

--- kernel/serial.c
+++ kernel/serial.c
@@ -1,6 +1,7 @@
 #include <mpx/io.h>
 #include <mpx/serial.h>
 #include <sys_req.h>
+#include <string.h>
 
 #define ENTER_KEY 10
 #define B_KEY 98
@@ -67,10 +68,13 @@ int serial_poll(device dev, char *buffer, size_t len)
 {
 	/* initialize pointer and index */
 	int index = 0;
+	// int cursor = index;
 	size_t count = len;
 		
 	// read char into data register
 	unsigned char data = inb(dev);
+	//increment index after collecting data
+	index++;
 	
 	// if char is enter key, new line
 	if (data == 13)
@@ -83,34 +87,56 @@ int serial_poll(device dev, char *buffer, size_t len)
 	}
 
 	 // backspace key
-	if (data == 127)
-	{
-		// buffer[index] = '\0';
-		outb(dev, '\b');   // Move the cursor back
-        outb(dev, ' ');    // Overwrite with a space
-        outb(dev, '\b');   // Move the cursor back again
-		index--;
+	if (data == 127) {  // Backspace key
+    	if (index > 0) 
+		{
+			outb(dev, '\b');   // Move the cursor back
+			outb(dev, ' ');    // Overwrite with a space
+			outb(dev, '\b');   // Move the cursor back again
+			index--;
+    	}
 	}
+
 
 	// delete key
-	if (data == 37)
+	if (data == 10)
 	{
 		// buffer[index] = '\0';
-		outb(dev, '\b');   // Move the cursor back
+		outb(dev, 'h');   // Move the cursor back
         outb(dev, ' ');    // Overwrite with a space
         outb(dev, '\b');   // Move the cursor back again
 		index--;
 	}
 
+	// escape scenarios
+	// unsigned char extra_data = inb(dev); 
+	// unsigned char extra_extra = inb(dev);
+	// if(data == '\033' && extra_data == '[' && extra_extra == 'D')
+	// {
+	// 	cursor = index;
+	// 	outb(dev, '\b');
+	// 	cursor--;
+	// 	unsigned char extra_data = inb(dev); 
+	// 	if (extra_data == '[')
+	// 	{
+	// 		unsigned char extra_extra = inb(dev);
+	// 		if (extra_extra == 'D')
+	// 		{
+	// 			cursor--;
+	// 			outb(dev, '\b');
+	// 		}
+	// 		if (extra_extra == 'C')
+	// 		{
+	// 			cursor++;
+	// 			outb(dev, '\b');
+	// 		}
+	// 	}
 	// write data to device and data to buffer array
 	outb(dev, data);
 	buffer[index] = data;
 
 	// decrement count
 	count--;
-
-	//increment index
-	index++;
 
 	return len - count;
 }
