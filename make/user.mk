c32396115efcbd6810237a9da3e1873720cb3b98
Ian Jackson <iansjackson8@gmail.com>
2023-08-30 20:12:44 -0400

    issue with time

--- make/user.mk
+++ make/user.mk
@@ -6,5 +6,6 @@ user/core.o: user/core.c include/string.h include/mpx/serial.h \
 USER_OBJECTS=\
 	user/core.o\
 	user/itoa.o\
-	user/version.o \
-	user/help.o
+	user/version.o\
+	user/time.o \
+	user/help.o 
