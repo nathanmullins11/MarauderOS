c8f86e1151e8387ced9e9140d618b958e6b09712
Ian Jackson <iansjackson8@gmail.com>
2023-09-04 19:20:27 -0400

    update getdate help and comhand

--- user/help.c
+++ user/help.c
@@ -7,16 +7,21 @@ void help(const char *cmd) {
     // build help strings for each command
     char msg_version[] = "version :\tdisplays the current version of MarauderOS and last build date\n";
     char msg_shutdown[] = "shutdown : \tquits MarauderOS\n";
+    char msg_getdate[] = "getdate : \tdisplays current date\n";
 
-    if ( strcmp(cmd, "version") == 0) {
+    if ( strcmp(cmd, "version") == 0 ) {
         // print help message for 'version' command
         sys_req(WRITE, COM1, msg_version, sizeof(msg_version));
-    } else if ( strcmp(cmd, "shutdown") == 0) {
+    } else if ( strcmp(cmd, "shutdown") == 0 ) {
         // print help message for 'shutdown' command
         sys_req(WRITE, COM1, msg_shutdown, sizeof(msg_shutdown));
+    } else if ( strcmp(cmd, "getdate") == 0 ) {
+        // print help message for 'getdate' command
+        sys_req(WRITE, COM1, msg_getdate, sizeof(msg_getdate));
     } else {
         // if not parameters -> print all help messages
         sys_req(WRITE, COM1, msg_version, sizeof(msg_version));
         sys_req(WRITE, COM1, msg_shutdown, sizeof(msg_shutdown));
+        sys_req(WRITE, COM1, msg_getdate, sizeof(msg_getdate));
     }
 }
