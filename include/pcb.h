bf1d2374e027ea502d78d78a1ca71dc66f05b596
nathanmullins11 <ndm00002@mix.wvu.edu>
2023-09-22 18:38:05 -0400

    setup pcb now works

--- include/pcb.h
+++ include/pcb.h
@@ -29,8 +29,8 @@ enum state {
 struct process {
         const char* process_name;
         int pcb_class; // class = 0 -> user app, class = 1 -> system process
-        int pcb_priority; // an int between 0 (high pri) and 9 (low pri)
         enum state pcb_state;
+        int pcb_priority; // an int between 0 (high pri) and 9 (low pri)
         char pcb_stack[1024]; // process stack of at least 1024
         void* stack_ptr; // pointer to current location in stack
         struct process* next_process; // related pcbs
