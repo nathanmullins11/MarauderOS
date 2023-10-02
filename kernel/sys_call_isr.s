2568751168e72a5195e7bd6ccc5e3ce034cafb87
Ian Jackson <iansjackson8@gmail.com>
2023-10-02 15:17:42 -0400

    assembly

--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -4,15 +4,12 @@ global sys_call_isr
 ;;; System call interrupt handler. To be implemented in Module R3.
 extern sys_call            ; The C function that sys_call_isr will call
 sys_call_isr:
-	cmp EAX, 1
-	je equal_to_one
+	push SS
+	push GS
+	push FS
+	push ES
+	push DS
 
-	jmp end_if
-
-
-	equal_to_one: 
-	; push reverse order of struct
-	push ESP
 	push EBP
 	push EDI
 	push ESI
@@ -20,30 +17,17 @@ sys_call_isr:
 	push ECX
 	push EBX
 	push EAX
-	push SS
-	push GS
-	push FS
-	push ES
-	push DS
-	
-	; push stack pointer
-	;; push ESP
+
+	push ESP
 
 	; call function
 	call sys_call
 
 	; set stack pointer based on return value 
-	mov ESP, EAX
+	mov EAX, ESP
 
-	; pop same order as struct
-
-	;; pop CS
-	pop DS
-	pop ES
-	pop FS
-	pop GS
-	pop SS
-
+	; pop registers in order of struct
+	pop ESP
 
 	pop EAX
 	pop EBX
@@ -52,12 +36,11 @@ sys_call_isr:
 	pop ESI
 	pop EDI
 	pop EBP
-	pop ESP
 
-	; mov EAX, 0
-	iret
-	jmp end_if
+	pop DS
+	pop ES
+	pop FS
+	pop GS
+	pop SS
 
-	end_if:
-	mov EAX, -1
 	iret
