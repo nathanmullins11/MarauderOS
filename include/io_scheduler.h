7c406f3b4be00f2a296a6d546febfb136f7ac969
rjstauffer <rileyjstauffer@gmail.com>
2023-11-17 13:31:13 -0500

    io scheduler

---
+++ include/io_scheduler.h
@@ -0,0 +1,10 @@
+/**
+ * @file io_scheduler.h
+ * @brief functions for IO scheduler
+ */
+
+int validate_io_request(struct context* context_ptr);
+
+
+void io_scheduler(struct context* context_ptr);
+
